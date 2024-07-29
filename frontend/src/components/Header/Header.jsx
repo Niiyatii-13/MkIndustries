d13e120a5ae2a848a2e8ca7cee20a3130e2a49b8
Niiyatii-13 <166405063+Niiyatii-13@users.noreply.github.com>
2024-07-29 23:23:15 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='header-contents'>
-                <h2>Order strong and reliable steel products here</h2>
+                <h2>Order reliable steel products here</h2>
                 <p>Choose from a diverse catalog featuring reliable products. Our mission is to enhance your experience, with great service and products.</p>
                 <a href = '#exploreMenu'>View Catalog</a>
             </div>
